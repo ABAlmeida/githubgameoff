e384d06e8be742fb8e9e389a0850b95b328d76d0
Antonio Almeida <Antonio.Almeida@codemasters.com>
2018-11-02 16:18:46 +0000

    Playing around with ghost platforms

---
+++ Assets/Ghost.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetAxis("Ghost") > 0.0f)
+        {
+            gameObject.SetActive(true);
+        }
+
+        if (Input.GetAxis("Ghost") == 0.0f)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
