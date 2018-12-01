d76174b5cff068fe2dbe89e0aa2725d02e885e4f
Antonio <antonio.almeida@coolgames.com>
2018-12-01 19:05:47 +0000

    Changed a few things.

---
+++ Assets/AnimationHack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationHack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Animator>().Play("Player_Idle");
+        Time.timeScale = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
